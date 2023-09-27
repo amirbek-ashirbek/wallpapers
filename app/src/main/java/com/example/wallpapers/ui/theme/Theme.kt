b67438c536c71b64d6d59c4523f388ef11c1c6f3
amirbek-ashirbek <amirbek.ashirbek@gmail.com>
2023-09-27 21:25:15 +0600

    Implemented theming (light and dark themes)

--- app/src/main/java/com/example/wallpapers/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wallpapers/ui/theme/Theme.kt
@@ -80,16 +80,6 @@ private val DarkColorScheme = darkColorScheme(
 	scrim = md_theme_dark_scrim,
 )
 
-	/* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-
 @Composable
 fun WallpapersTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
